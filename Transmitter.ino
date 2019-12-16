ded2d0114a3f6257e253c939f9ce81033f372987
Adithya Anilkumar <53343483+adithyaanilkumar@users.noreply.github.com>
2019-12-16 23:09:16 +0530

    Create Transmitter.ino

---
+++ Transmitter.ino
@@ -0,0 +1,37 @@
+
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setTxPower(20);
+  
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("help needed at  ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
+
+ 
